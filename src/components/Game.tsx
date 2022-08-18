fda3af456776257f94c41f14001b3fef6d3c0848
Demir Boraj <demirboraj97@gmail.com>
2022-08-18 19:17:16 +0200

    building the game board

---
+++ src/components/Game.tsx
@@ -0,0 +1,24 @@
+export function Game() {
+  return (
+        <div className="game-board">
+
+            <div className="game-board-row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            </div>
+
+            <div className="game-board-row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            </div>
+
+            <div className="game-board-row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            </div>
+            </div>
+  );
+}
